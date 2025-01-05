ad8c0b518b749fe5b3de3fe1e5d016a63e1535ab
Swetha <swethakakar@gmail.com>
2025-01-05 11:03:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
-function firstNonRepeatedChar(str) {
+function firstNonRepeatedChar(sr) {
 // your code here
     let m=new Map();
     let n=str.length;
     for(let i=0; i<n; i++)
     {
         let k=0;
-        if(m.get(s[i])){
-            k=m.get(s[i]);
+        if(m.get(str[i])){
+            k=m.get(str[i]);
         }
-        m.set(s[i],k+1);
+        m.set(str[i],k+1);
     }
     let ans=null;
     for(let i=0; i<n; i++)
     {
-        if(m.get(s[i]==1)){
-            ans=s[i];
+        if(m.get(str[i]==1)){
+            ans=str[i];
             break;
         }
     }
